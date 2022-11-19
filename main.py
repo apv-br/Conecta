5133f61d0a7ab50b8b0a14b92d39cadd82906f18
apv-br <110625373+apv-br@users.noreply.github.com>
2022-11-19 15:56:37 +0000

    first publication
    first publication

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+from flask import Flask, render_template, make_response
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.route("/oquefazemos")
+def oquefazemos():
+    return render_template("oquefazemos.html")
 
+@app.route("/nossaspublicacoes")
+def nossaspublicacoes():
+    return render_template("nossaspublicacoes.html")
 
-# Press the green button in the gutter to run the script.
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template("quemsomos.html")
+
+@app.route("/faleconosco")
+def faleconosco():
+    return render_template("faleconosco.html")
+
+# ---------- posts --------------
+
+@app.route("/post_page_2")
+def post_2():
+    post_id = "2"
+    response = make_response(render_template("post_page.html", post=post_id))
+    return response
+
+@app.route("/post_page_1")
+def post_1():
+    post_id = "1"
+    response = make_response(render_template("post_page.html", post=post_id))
+    return response
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(use_reloader=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
